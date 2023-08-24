32abfb2cae1d62fd20eadd385eb694003c9b4c52
David Florez <dflorez6@gmail.com>
2023-08-24 17:45:19 -0400

    1.0.0.4
    # REACT INTRO #
    
    ## Front-End ##
    *  Class 6 - React Events: onClick, onChange
    
    ## Back-End ##
    *
    
    ## Pending ##
    *

--- src/components/CreateTodoButton/CreateTodoButton.jsx
+++ src/components/CreateTodoButton/CreateTodoButton.jsx
@@ -4,7 +4,17 @@ import './CreateTodoButton.css'; // Import CSS Styles from external file
 // Component: CreateTodoButton
 function CreateTodoButton() {
     return(
-        <button className='create-todo'>+</button> 
+        <button 
+            className='create-todo'
+            onClick={
+                // JS code must be encapsulated inside a function
+                (event) => {
+                    console.log('onClick est');
+                    console.log(event);
+                    console.log(event.target);
+                }
+            }
+        >+</button>
     );
 }
 
--- src/components/TodoSearch/TodoSearch.jsx
+++ src/components/TodoSearch/TodoSearch.jsx
@@ -5,7 +5,18 @@ import './TodoSearch.css'; // Import CSS Styles from external file
 function TodoSearch() {
     return(
         <div className='form-group'>
-            <input type="search" name="" id="" placeholder="Find Task" className='form-control' />
+            <input 
+                type="search" name="" id="" placeholder="Find Task" className='form-control'
+                onChange={
+                    // JS code must be encapsulated inside a function
+                    (event) => {
+                        // console.log('onChange Test');
+                        // console.log(event);
+                        // console.log(event.target);
+                        console.log(event.target.value);
+                    }
+                }
+            />
         </div>        
     );
 }
